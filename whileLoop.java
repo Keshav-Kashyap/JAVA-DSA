911572c0a79fee9781d9a5014dc75b441eb4bd43
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-07-30 23:56:19 +0530

    while loop

---
+++ whileLoop.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class whileLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 10;
+        while (n > 0) {
+            System.out.println("Hello World");
+            n--;
+        }
+
+    }
+}
