81f3c1c1288d12c2bac1a9b34cd34a0dae9e419d
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-08-05 23:41:43 +0530

    methods

---
+++ funx.java
@@ -0,0 +1,11 @@
+public class funx {
+
+    public static void hello() {
+        System.out.println("hello Methods");
+    }
+
+    public static void main(String[] args) {
+        hello();
+
+    }
+}
