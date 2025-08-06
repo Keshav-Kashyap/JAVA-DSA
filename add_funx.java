f4d014102b3ca0ede58593ae04250e1cba0c9290
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-08-06 23:54:18 +0530

    add funx

---
+++ add_funx.java
@@ -0,0 +1,14 @@
+public class add_funx {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        int result = add(10, 20);
+
+        System.out.println("Sum of a and b:" + result);
+
+    }
+}
