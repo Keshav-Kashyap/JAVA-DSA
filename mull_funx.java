bc4e9efdf62b4a36bce7e3969b4f62432ab1ef36
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-08-07 23:53:57 +0530

    mul

---
+++ mull_funx.java
@@ -0,0 +1,14 @@
+public class mull_funx {
+
+    public static int mul(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+
+        int result = mul(10, 20);
+
+        System.out.println("Sum of a and b:" + result);
+
+    }
+}
