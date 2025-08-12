49af323ea215c6481815eed568d601fef09a3b86
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-08-12 23:54:24 +0530

    chnages

--- Day 16/Hollow_Rect.java
+++ Day 16/Hollow_Rect.java
@@ -2,23 +2,30 @@ import java.util.Scanner;
 
 public class Hollow_Rect {
 
+    public static void hollow_rectangle(int totRows, int totCols) {
+
+        for (int i = 1; i <= totRows; i++) {
+            for (int j = 1; j <= totCols; j++) {
+                if (i == 1 || i == totRows || j == 1 || j == totCols) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your Number");
-        int num = sc.nextInt();
-
-        for (int i = 1; i <= num; i++) {
-
-            for (int j = 1; j <= num; j++) {
-                if (i == 1 || i == num || j == 1 || j == num) {
-                    System.out.print("*");
-
-                }
-                System.out.println();
-            }
-
-        }
+        System.out.println("Enter your total Rows");
+        int totRows = sc.nextInt();
+        System.out.println("Enter your total Columns");
+        int totCols = sc.nextInt();
+        hollow_rectangle(totRows, totCols);
 
     }
 
