c77e23308cc116bf4052fba58d530d4f6373b3ec
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-08-15 23:51:31 +0530

    chnage in Zero_one

--- Day 19/Zero_One.java
+++ Day 19/Zero_One.java
@@ -12,12 +12,11 @@ public class Zero_One {
         for (int i = 1; i <= n; i++) {
 
             for (int j = 1; j <= i; j++) {
-                if (count == 0) {
-                    count = 1;
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1 ");
                 } else {
-                    count = 0;
+                    System.out.print("0 ");
                 }
-                System.out.print(" " + count);
 
             }
 
