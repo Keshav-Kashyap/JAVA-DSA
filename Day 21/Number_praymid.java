4397bd59ecf8b31cfa5f9047a362074e85000557
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-08-17 23:51:28 +0530

    done

--- Day 21/Number_praymid.java
+++ Day 21/Number_praymid.java
@@ -18,7 +18,12 @@ public class Number_praymid {
             for (int j = 1; j <= i; j++) {
                 System.out.print(i);
 
+                for (int k = 1; k <= 1; k++) {
+                    System.out.print(" ");
+                }
+
             }
+
             System.out.println();
         }
 
