fb65fdd8b60425fce9dda73f9cf0e6a98a5c6054
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-08-25 23:24:50 +0530

    today nohing

---
+++ Day 29/man.java
@@ -0,0 +1,6 @@
+
+public class man {
+    public staic void main(String[]args){
+        System.out.println("Aaj thoda extra kaam hai isliye code nhi kar paaya but striek manintain rakhan ahi ");
+    }
+}
