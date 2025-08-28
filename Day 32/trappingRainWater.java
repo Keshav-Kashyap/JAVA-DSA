05165f115b6bfa2344e8ed53127fdeec6e7bfc45
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-08-28 23:25:46 +0530

    tarpping Rain Water day 32

---
+++ Day 32/trappingRainWater.java
@@ -0,0 +1,12 @@
+public class trappingRainWater {
+
+    public static int trappedRainwater(int height[]) {
+        // first 1. Calculate Left Max Boundary ->
+
+    }
+
+    public static void main(String[] args) {
+        int height[] = { 4, 2, 0, 6, 3, 2, 5 };
+
+    }
+}
