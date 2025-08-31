38646e9ea2275c58f5eda3e3c35b279ef2264af0
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-09-01 00:00:21 +0530

    ok

---
+++ Day 35/BubbleSort.java
@@ -0,0 +1,26 @@
+public class BubbleSort {
+
+    public static void bubbleSort(int arr[]) {
+
+        for (int i = 0; i <= arr.length; i++) {
+
+            if (arr[i] < arr[i + 1]) {
+
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+
+            }
+
+        }
+
+        bubbleSort(arr);
+
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = { 4, 5, 3, 7, 1, 2 };
+
+    }
+}
