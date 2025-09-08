66d63271a5e03dcd30650df863b3f477f4077301
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-09-08 23:39:02 +0530

    day 8

---
+++ Day 44/countngSort.java
@@ -0,0 +1,18 @@
+public class countngSort {
+
+    public static void countingSort(int arr[]) {
+
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+
+        }
+
+        int count[] = new int[largest + 1];
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
