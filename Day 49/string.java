c6a90f0d2154819cf93fcb236ed7d40321b39ca7
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-09-13 23:38:16 +0530

    done

---
+++ Day 49/string.java
@@ -0,0 +1,9 @@
+public class string {
+    public static void main(String[] args) {
+
+        char arr[] = { 'a', 'b', 'c' };
+        String str = "ABCD";
+        String str2 = new String("xyz");
+
+    }
+}
