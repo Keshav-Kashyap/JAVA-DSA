926df8833cd5836c8653b1e5f91abf4c65deabd3
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-09-23 23:42:33 +0530

    day 23

--- Day 56/mergeSort.java
+++ Day 56/mergeSort.java
@@ -41,7 +41,7 @@ public class mergeSort {
 
     public static void mergesort(int arr[], int start, int end) {
 
-        if (start <= end) {
+        if (start < end) {
 
             int mid = start + (end - start) / 2;
 
