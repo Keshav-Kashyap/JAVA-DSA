0bf75e97a02e7937284717a79cdd8c91128a861f
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-09-23 23:41:40 +0530

    day 23

---
+++ Day 59/gettersAndSetters.java
@@ -0,0 +1,37 @@
+public class gettersAndSetters {
+
+    public static void main(String[] args) {
+
+        Pen p1 = new Pen();
+        p1.setColor("blue");
+        p1.setTip(5);
+
+        System.out.println(p1.getColor());
+        System.out.println(p1.getTip());
+
+    }
+
+}
+
+class Pen {
+    private String color;
+    private int tip;
+
+    String getColor() {
+        return this.color;
+    }
+
+    int getTip() {
+        return this.tip;
+    }
+
+    void setColor(String color) {
+
+        this.color = color;
+    }
+
+    void setTip(int tip) {
+        this.tip = tip;
+    }
+
+}
