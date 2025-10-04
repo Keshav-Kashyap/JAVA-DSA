26bfbe3bd7bdfc96c16755f78666db469754c5ae
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-10-04 22:26:29 +0530

    done

---
+++ Day 70/tilingProblem.java
@@ -0,0 +1,14 @@
+public class tilingProblem {
+
+    public static void Tilling(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
