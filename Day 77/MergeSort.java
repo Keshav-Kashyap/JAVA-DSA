170fb6e609d9b98a1ac4397366ced377af86d06a
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-10-11 23:08:22 +0530

    done

---
+++ Day 77/MergeSort.java
@@ -0,0 +1,61 @@
+public class MergeSort {
+
+    public static void mergeSort(int arr[], int si, int ei) {
+        if (si >= ei) {
+            return;
+        }
+
+        int mid = si + (ei - si) / 2;
+
+        // left
+        mergeSort(arr, si, mid);
+        // right
+        mergeSort(arr, mid + 1, ei);
+
+        // merge
+        merge(arr, si, mid, ei);
+
+    }
+
+    public static void merge(int arr[], int si, int mid, int ei) {
+
+        // temp array
+        int temp[] = new int[ei - si + 1];
+        int i = si;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= ei) {
+            if (arr[i] < arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= ei) {
+            temp[k++] = arr[j++];
+        }
+
+        for (k = 0, i = si; k < temp.length; k++, i++) {
+            arr[i] = temp[k];
+        }
+
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 1, 3, 2 };
+        mergeSort(arr, 0, arr.length - 1);
+        printArr(arr);
+    }
+}
