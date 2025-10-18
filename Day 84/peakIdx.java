3757b9ce54b52de011cd3ea2c71b2b4cfad6febf
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-10-18 23:53:47 +0530

    done

--- Day 84/peakIdx.java
+++ Day 84/peakIdx.java
@@ -1,7 +1,33 @@
 public class peakIdx {
+
+    public static int FindPeak(int arr[]) {
+
+        int si = 0;
+
+        int ei = arr.length - 1;
+
+        while (si < ei) {
+            int mid = si + (ei - si) / 2;
+
+            if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) {
+                return mid;
+            } else if (arr[mid - 1] < arr[mid]) {
+                si = mid + 1;
+
+            } else {
+                ei = mid;
+
+            }
+
+        }
+
+        return si;
+
+    }
+
     public static void main(String[] args) {
 
         int[] arr = { 0, 3, 8, 9, 5, 2 };
-
+        System.out.println(FindPeak(arr));
     }
 }
