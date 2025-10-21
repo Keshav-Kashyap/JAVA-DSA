dfccbaa5816015b3ebd25e92e8ebf390d4949919
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-10-21 23:33:32 +0530

    done

--- Day 87/bookAllocation.java
+++ Day 87/bookAllocation.java
@@ -1,9 +1,57 @@
-public class bookAllocation{
+public class BookAllocation {
+
+    public static boolean isPossible(int[] pages, int n, int m, int mid) {
+        int studentCount = 1;
+        int pageSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (pages[i] > mid)
+                return false;
+
+            if (pageSum + pages[i] > mid) {
+                studentCount++;
+                pageSum = pages[i];
+                if (studentCount > m)
+                    return false;
+            } else {
+                pageSum += pages[i];
+            }
+        }
+
+        return true;
+    }
+
+    public static int allocateBooks(int[] pages, int n, int m) {
+        if (m > n)
+            return -1; // more students than books → not possible
+
+        int start = 0;
+        int sum = 0;
+        for (int page : pages)
+            sum += page;
+
+        int end = sum;
+        int ans = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (isPossible(pages, n, m, mid)) {
+                ans = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
+        int[] pages = { 12, 34, 67, 90 };
+        int n = pages.length;
+        int m = 2;
 
-        int []arr = {2,3,4,5};
-        
-
-
+        System.out.println("Minimum Maximum Pages " + allocateBooks(pages, n, m));
     }
 }
