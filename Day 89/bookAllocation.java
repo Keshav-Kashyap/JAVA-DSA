ab6e211544e34beef62218f505f850d0de13db9a
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-10-24 23:28:35 +0530

    done

--- Day 89/bookAllocation.java
+++ Day 89/bookAllocation.java
@@ -1,8 +1,56 @@
-public class bookAllocation{
-    public static void main(String[]args){
+import java.util.Arrays;
 
+public class bookAllocation {
 
+    public static int bookAll(int arr[], int m) {
 
-        
+        int si = 0;
+        int ei = Arrays.stream(arr).sum();
+        int ans = 0;
+        while (si <= ei) {
+            int mid = si + (ei - si) / 2;
+
+            if (isVaild(mid, arr, m)) {
+                ans = mid;
+                ei = mid - 1;
+            } else {
+                si = mid + 1;
+            }
+        }
+
+        return ans;
+
+    }
+
+    public static boolean isVaild(int mid, int arr[], int m) {
+
+        int students[] = new int[m];
+        int st = 0;
+        for (int i = 0; i < arr.length; i++) {
+
+            if ((students[st] + arr[i]) <= mid) {
+
+                students[st] += arr[i];
+            } else {
+                if (st < students.length - 1) {
+                    st++;
+                    i--;
+                } else {
+                    return false;
+                }
+            }
+
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = { 2, 1, 3, 4 };
+        int m = 2;
+
+        System.out.println(bookAll(arr, m));
+
     }
 }
---
+++ Day 89/check.java
@@ -0,0 +1,10 @@
+public class check {
+    public static void main(String[] args) {
+
+        int n = 4;
+        int arr[] = new int[n];
+        int variable = 5;
+        System.out.println(arr[0]);
+
+    }
+}
