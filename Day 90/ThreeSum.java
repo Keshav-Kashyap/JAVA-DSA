eeebe189c2065a5b1395ef59880f75b733a1b671
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-10-26 23:28:14 +0530

    done

---
+++ Day 90/ThreeSum.java
@@ -0,0 +1,16 @@
+public class ThreeSum {
+
+    public static void main(String[] args) {
+        
+
+        int arr[] = {-1,0,1,2,-1,-4};
+
+        for(int i = 0 ; i<arr.length-2;i++)
+
+
+
+
+
+    }
+
+}
---
+++ Day 91/backTracking.java
@@ -0,0 +1,18 @@
+public class backTracking {
+
+    public static void MakeArray(int arr[], int i) {
+
+        if (i == arr.length - 1) {
+
+            return;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = new int[5];
+
+    }
+
+}
