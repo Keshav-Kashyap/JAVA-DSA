4ce45490d66f793d74e93e1cfa509aeecd4cdf8c
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-10-26 23:32:59 +0530

    done

--- Day 91/backTracking.java
+++ Day 91/backTracking.java
@@ -2,17 +2,32 @@ public class backTracking {
 
     public static void MakeArray(int arr[], int i) {
 
-        if (i == arr.length - 1) {
+        if (i == arr.length) {
 
             return;
         }
 
+        arr[i] = i + 1;
+
+        MakeArray(arr, i + 1);
+
+        // backTracking
+        arr[i] = arr[i] - 2;
+
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 
     public static void main(String[] args) {
 
         int arr[] = new int[5];
 
+        MakeArray(arr, 0);
+        printArr(arr);
     }
 
 }
