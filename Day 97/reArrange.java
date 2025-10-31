158826585bb2026fca68d529bd8b04aa22995915
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-10-31 23:42:59 +0530

    done

--- Day 97/reArrange.java
+++ Day 97/reArrange.java
@@ -1,10 +1,25 @@
 public class reArrange {
- public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    int nums = [3,1,-2,-5,2,-4]
-//Output: [3,-2,1,-5,2,-4]
+        int nums[] = { 3, 1, -2, -5, 2, -4 };
+        int newNums[] = new int[nums.length];
+        int pos = 0, neg = 1;
 
+        for (int i = 0; i < nums.length; i++) {
 
+            if (nums[i] > 0) {
+                newNums[pos] = nums[i];
+                pos += 2;
+            } else {
+                newNums[neg] = nums[i];
+                neg += 2;
+            }
 
- }
+        }
+
+        for (int i = 0; i < newNums.length; i++) {
+            System.out.println(newNums[i]);
+        }
+
+    }
 }
