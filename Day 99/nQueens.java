281b97c7c9bf60df091ee47f9c31cd7aae6581ca
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-11-03 23:52:29 +0530

    done

--- Day 99/nQueens.java
+++ Day 99/nQueens.java
@@ -1,14 +1,25 @@
 public class nQueens {
 
-public static void nQueen(int board[][], int  row , int n){
-    if(row == n){
-    printBoard(board[][]);
-    return;
+    public static void nQueen(char board[][], int row, int n) {
+        if (row == n) {
+            printBoard(board);
+            return;
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (isSafe(board[][],row,j,n)) {
+
+                board[row][j] = 'Q';
+                nQueen(board, row + 1, n);
+                board[row][j] = '.';
+
+            }
+
+        }
 
     }
-}
 
-    public static void printBoard(int board[][]) {
+    public static void printBoard(char board[][]) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
 
@@ -19,9 +30,46 @@ public static void nQueen(int board[][], int  row , int n){
         }
     }
 
+    public static boolean isSafe(char board[][], int row, int col, int n) {
+
+        // same row
+
+        for (int j = 0; j < n; j++) {
+            if (board[row][j] == 'Q')
+                return false;
+        }
+
+        // same column
+
+        for (int i = 0; i < n; i++) {
+            if (board[i][col] == 'Q')
+                return false;
+
+        }
+        // Left up Digonal
+
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+
+            if (board[i][j] == 'Q')
+                return false;
+
+        }
+        // Right Dignoal
+        for (int i = row, j = col; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        // kisi main bhi nhi gye bhai to safe hai
+
+        return true;
+
+    }
+
     public static void main(String[] args) {
 
-        int board[][] = new int[4][4];
+        char board[][] = new char[4][4];
 
+        nQueen(board, 0, 4);
     }
 }
