93175bb07484bde4116969e3ff6fc7ca36343104
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-11-11 23:30:00 +0530

    done

---
+++ Day 106/containerWithMostWater.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class containerWithMostWater {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> height = new ArrayList<>();
+        height.add(1);
+        height.add(8);
+        height.add(6);
+        height.add(2);
+        height.add(5);
+        height.add(4);
+        height.add(8);
+        height.add(3);
+        height.add(7);
+
+        int waterContains = 0;
+
+        for (int i = 0; i < height.size(); i++) {
+            for (int j = i + 1; j < height.size(); j++) {
+
+                int width = j - i;
+                int heightOf = Math.min(height.get(i), height.get(j));
+                if (waterContains < heightOf * width)
+                    waterContains = heightOf * width;
+
+            }
+        }
+        System.out.println(waterContains);
+
+    }
+
+}
