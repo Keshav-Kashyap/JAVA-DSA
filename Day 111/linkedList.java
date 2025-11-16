801c681e2c23e347533adbe78776958f68aefc96
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-11-16 23:54:48 +0530

    done

--- Day 111/linkedList.java
+++ Day 111/linkedList.java
@@ -1,7 +1,71 @@
-public class linkedList{
-    public static void main(String[]args){
+public class LinkedList {
 
+    // creating a class
+    // data and next-> it store the another obj(node type )
 
-        
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    // methods
+    // add in linkedlist
+    // add First and Add Last
+
+    // add First
+    // create a newNode
+    // newNode next->head
+    // head->newNode
+
+    public void addFirst(int data) {
+
+        // constant time O(1)
+        // create a newNode
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        // newNode.next->head
+        newNode.next = head;
+        // head->newNode
+        head = newNode;
+
+    }
+
+    // add last
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = tail;
+        tail = newNode;
+
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+
+        ll.addLast(66);
+        ll.addLast(65);
+        ll.addLast(64);
+        ll.addLast(63);
     }
 }
