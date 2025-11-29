de96eeea971773e51f9f8f39ca58ff346859e099
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-11-29 23:05:47 +0530

    done

---
+++ Day 124/StackC.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class StackC {
+
+    public static int stockSpan(int stocks[], int span[]) {
+
+
+            Stack<Integer> s = new Stack<>();
+            span[0] = 1;
+            s.push(0);
+            for(int i = 1; i<stocks.length; i++){
+                
+            }
+
+    }
+
+    public static void main(String[] args) {
+        int stocks[] = { 100, 80, 60, 70, 60, 85, 100 };
+        int span[] = new int[stocks.length];
+        stockSpan(stocks, span);
+        for (int i = 0; i < span.length; i++) {
+            System.out.println(span[i]);
+        }
+
+    }
+
+}
---
+++ Day 125/stackB.java
@@ -0,0 +1,13 @@
+import java.util.Stack;
+
+public class stackB{
+    public static void main(String[] args) {
+        
+        Stack <Integer> s = new Stack<>();
+        
+
+
+
+
+    }
+}
