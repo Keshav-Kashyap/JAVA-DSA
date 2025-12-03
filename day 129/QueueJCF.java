7c1a360faf096afd7514118908f92314cda1af57
keshavops <keshav@oneplacesolar.com>
2025-12-03 23:16:03 +0530

    done

--- day 129/QueueJCF.java
+++ day 129/QueueJCF.java
@@ -2,8 +2,15 @@ import java.util.*;
 
 public class QueueJCF {
     public static void main(String[] args) {
-        Queue<Integer> q = new Queue<Integer>() {
-            
-        };
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(14);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
     }
 }
