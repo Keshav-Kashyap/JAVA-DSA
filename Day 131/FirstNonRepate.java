2629a03328d01808b73b65b728089919ffdbdded
keshavops <keshav@oneplacesolar.com>
2025-12-05 23:38:23 +0530

    done

---
+++ Day 131/FirstNonRepate.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class FirstNonRepate {
+
+    public static void firstNonRepeate(String str) {
+
+        int map[] = new int[str.length()];
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            q.add(str.charAt(i));
+            if()
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
