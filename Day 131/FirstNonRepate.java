4bcdcba7d1aae2225be4bc18ac432fe36edfe466
keshavops <keshav@oneplacesolar.com>
2025-12-05 23:56:39 +0530

    done

--- Day 131/FirstNonRepate.java
+++ Day 131/FirstNonRepate.java
@@ -5,18 +5,30 @@ public class FirstNonRepate {
 
     public static void firstNonRepeate(String str) {
 
-        int map[] = new int[str.length()];
-        Queue<Integer> q = new LinkedList<>();
+        int map[] = new int[26];
+        Queue<Character> q = new LinkedList<>();
 
         for (int i = 0; i < str.length(); i++) {
-            q.add(str.charAt(i));
-            if()
+            char ch = str.charAt(i);
+            q.add(ch);
+            map[ch - 'a']++;
+            while (!q.isEmpty() && map[q.peek() - 'a'] > 1) {
+                q.remove();
+
+            }
+
+            if (q.isEmpty()) {
+                System.out.println(-1);
+            } else {
+                System.out.println(q.peek() + " ");
+            }
 
         }
 
     }
 
     public static void main(String[] args) {
-
+        String str = "aabccxb";
+        firstNonRepeate(str);
     }
 }
